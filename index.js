d1f8402c9c833aca447efc27c2bbef203d50b9c1
beatriz <beatrizffeo@gmail.com>
2024-03-12 23:42:42 -0600

    lab complete

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+const randomNum = Math.random();
+const num3 = 24;
+const num4 = 5;
+const mod = num3 % num4;
+const max = Math.max(1, 2, 3, 4, 5, 20);
--- test/indexTest.js
+++ test/indexTest.js
@@ -1,22 +1,36 @@
-require ( './helpers.js' );
+// require ( './helpers.js' );
 
-const chai = require('chai')
+// const chai = require('chai')
+// const expect = chai.expect;
+
+// describe('index.js', function() {
+//   describe('multiply', function() {
+  require('./helpers.js');
+
+const chai = require('chai');
 const expect = chai.expect;
 
 describe('index.js', function() {
-  describe('multiply', function() {
     it("is an equation whose multiplied results will equal 62", function() {
       expect(num1).to.be.a('number')
       expect(num2).to.be.a('number')
       expect(multiply).to.eq(62)
     });
   });
+
   describe('random', function() {
-    it("generates a random integer greater than 0", function() {
-      expect(Number.isInteger(random)).to.be.true
-      expect(random).to.not.eq(0)
+    it("generates a random number greater than 0", function() {
+      expect(randomNum).to.be.a('number')
+      expect(randomNum).to.be.above(0)
     });
   });
+  
+  // describe('random', function() {
+  //   it("generates a random integer greater than 0", function() {
+  //     expect(Number.isInteger(random)).to.be.true
+  //     expect(random).to.not.eq(0)
+  //   });
+  // });
   describe('mod', function() {
     it("is an equation that calculates a remainder that is equal to 4", function() {
       expect(num3).to.be.a('number')
@@ -29,4 +43,3 @@ describe('index.js', function() {
       expect(max).to.eq(20)
     });
   });
-});
